25083d857e00be3ed97b6636cec83d9040237ac4
Edgar Martinez <admin@orchid1software.com>
2013-05-07 17:57:54 -0700

    cleaning up

--- app/js/main.js
+++
@@ -1,7 +0,0 @@
-/**
- * Created with JetBrains PhpStorm.
- * User: Edgar Martinez
- * Date: 5/7/13
- * Time: 11:34 AM
- * To change this template use File | Settings | File Templates.
- */
--- test/spec/app.Spec.js
+++
@@ -1,8 +0,0 @@
-define([ 'jquery', 'angular'], function($, angular) {
-    describe('just checking', function() {
-        it('works for underscore', function() {
-            // just checking that _ works
-            expect(3).toEqual(3);
-        });
-    });
-});
