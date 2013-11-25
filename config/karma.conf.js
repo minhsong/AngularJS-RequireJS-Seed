c01575b5b292be244bbf2e01620dd6893f51e73e
Brian Wilson <brianwilson0128@gmail.com>
2013-11-25 15:30:34 -0800

    Adapted to conform to newer format

--- config/karma.conf.js
+++ config/karma.conf.js
@@ -1,23 +1,56 @@
-basePath = '../';
 
-files = [
-    JASMINE,
-    JASMINE_ADAPTER,
-    REQUIRE,
-    REQUIRE_ADAPTER,
-    {pattern:'app/lib/angular/angular.js', included:false},
-     {pattern:'app/lib/angular/angular-resource.js', included:false},
-    {pattern:'test/lib/angular/angular-mocks.js', included:false},
-    {pattern:'app/js/**/*.js', included:false},
-    {pattern:'test/unit/**/*.js', included:false},
-    'test/main-test.js'
-];
+module.exports = function(config) {
+    config.set({
+        // base path, that will be used to resolve files and exclude
+        basePath: '../',
 
-autoWatch = true;
+        // testing framework to use (jasmine/mocha/qunit/...)
 
-browsers = ['Chrome'];
+        frameworks: ['jasmine',
+            'requirejs'
+        ],
 
-junitReporter = {
-  outputFile: 'test_out/unit.xml',
-  suite: 'unit'
+
+        // list of files / patterns to load in the browser
+        files: [
+            {pattern:'app/lib/angular/angular.js', included:false},
+            {pattern:'app/lib/angular/angular-resource.js', included:false},
+            {pattern:'test/lib/angular/angular-mocks.js', included:false},
+            {pattern:'app/js/**/*.js', included:false},
+            {pattern:'test/unit/**/*.js', included:false},
+            'test/main-test.js'
+        ],
+        // list of files / patterns to exclude
+        exclude: ['app/scripts/main.js'],
+
+        reporters: ['progress'],
+
+        // web server port
+        port: 8080,
+
+        // level of logging
+        // possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
+        logLevel: config.LOG_INFO,
+
+
+        // enable / disable watching file and executing tests whenever any file changes
+        autoWatch: false,
+
+
+        // Start these browsers, currently available:
+        // - Chrome
+        // - ChromeCanary
+        // - Firefox
+        // - Opera
+        // - Safari (only Mac)
+        // - PhantomJS
+        // - IE (only Windows)
+        browsers: ['Chrome'],
+
+
+        // Continuous Integration mode
+        // if true, it capture browsers, run tests and exit
+        singleRun: true
+    });
 };
+
