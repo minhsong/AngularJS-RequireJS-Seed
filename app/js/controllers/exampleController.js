cd89328900e650334d9aca021e5601fd066840e7
Edgar Martinez <admin@orchid1software.com>
2013-11-28 12:40:26 -0800

    Added test for exampleController

--- app/js/controllers/exampleController.js
+++ app/js/controllers/exampleController.js
@@ -2,14 +2,13 @@
  * Created by Edgar Martinez on 11/28/13.
  */
 define(function () {
-    var ExampleController = function ($scope, galleryService, $routeParams, $timeout, exampleService){
+    var ExampleController = function ($scope){
         //all your controller functionality in here / logic
 
         $scope.movie = {name: "Batman"};
-        $scope.exampleList = exampleService.get();
     };
 
-    ExampleController.$inject = ["$scope", "galleryService", "$routeParams", "$timeout", "exampleService"];
+    ExampleController.$inject = ["$scope"];
 
     return ExampleController;
 });
