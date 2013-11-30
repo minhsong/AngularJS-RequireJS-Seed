2a0059048ca8fdab4b05acc11d7185f63476bb0b
Edgar Martinez <admin@orchid1software.com>
2013-11-30 13:57:32 -0800

    refactored

--- tests/e2e/site/fontEndSpec.js
+++ tests/e2e/site/fontEndSpec.js
@@ -3,10 +3,6 @@ describe("Font end tests", function () {
        browser().navigateTo('../../index.html');
    });
 
-    it("Control class should exist", function () {
-        expect(element(".control").count()).toBe(1);
-    });
-
     it("Ngbk Input should have focus", function () {
         setTimeout(function () {
             expect(element(".focus").count()).toBe(1);
