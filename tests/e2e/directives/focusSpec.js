851e69d955b8491778fec518063fe4e4fb87805c
Edgar Martinez <admin@orchid1software.com>
2013-12-04 12:13:25 -0800

    small refactors

--- tests/e2e/directives/focusSpec.js
+++ tests/e2e/directives/focusSpec.js
@@ -1,12 +1,15 @@
 describe("e2e", function () {
     beforeEach(function () {
-        browser().navigateTo("../../app/index.html");
+        browser().navigateTo("index.html");
+        sleep(2);
     });
 
-    it("Should start", function () {
-        setTimeout(function () {
-            expect(element('.player-widget').count().toBe(3));
-        }, 100)
-        sleep(1);
-    })
-}) ;
+
+        it("Should start", function () {
+
+            expect(element('.focus').count()).toBe(1);
+
+        })
+
+
+});
