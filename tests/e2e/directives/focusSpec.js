1a8835b31983370740abcccba2c695cd9ca5f753
Edgar Martinez <admin@orchid1software.com>
2013-12-05 07:44:27 -0800

    Added protractor

--- tests/e2e/directives/focusSpec.js
+++
@@ -1,9 +0,0 @@
-describe("e2e", function () {
-    beforeEach(function () {
-        browser().navigateTo("index.html");
-
-    });
-    it("Should start", function () {
-        expect(element('.focus').count()).toBe(1);
-    })
-});
