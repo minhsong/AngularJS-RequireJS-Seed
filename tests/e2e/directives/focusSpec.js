26e58eb063f0f491f58f131e705d53f0630c15d0
Edgar Martinez <admin@orchid1software.com>
2013-12-04 20:00:09 -0800

    removed the ng-scenario based e2e testing

--- tests/e2e/directives/focusSpec.js
+++ tests/e2e/directives/focusSpec.js
@@ -1,15 +1,9 @@
 describe("e2e", function () {
     beforeEach(function () {
         browser().navigateTo("index.html");
-        sleep(2);
+
     });
-
-
-        it("Should start", function () {
-
-            expect(element('.focus').count()).toBe(1);
-
-        })
-
-
+    it("Should start", function () {
+        expect(element('.focus').count()).toBe(1);
+    })
 });
